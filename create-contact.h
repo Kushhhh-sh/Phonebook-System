2e7147f999960e48e2b17763eb0be0168a2fd198
Kush Agarwal <agarwalkushajay@gmail.com>
2022-01-16 14:36:25 +0530

    Added Add Contact Feature

---
+++ create-contact.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+class CreateContact
+{
+private:
+    string contactName;
+    long long int *mobileNumber;
+    int no_of_contacts;
+
+public:
+    void getDetails()
+    {
+        system("cls");
+        cout << "Enter the Name of the contact: ";
+        getline(cin >> ws, contactName);
+        cout << "How many Phone numbers would you like to save under Contact name " << contactName << "? : ";
+        cin >> no_of_contacts;
+        mobileNumber = (long long int *)malloc(sizeof(long long int) * no_of_contacts);
+        cout << "Enter the Contact Numbers: \n";
+        for (int i = 0; i < no_of_contacts; i++)
+            cin >> mobileNumber[i];
+    }
+
+    bool confirmDetails()
+    {
+        string decision;
+        cout << "You have Entered: " << contactName << " - ";
+        for (int i = 0; i < no_of_contacts; i++)
+        {
+            cout << mobileNumber[i] << " / ";
+        }
+        cout << "\nAre you sure? (Yes/No): ";
+        cin >> decision;
+        if (decision == "Yes")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void addContact()
+    {
+        system("cls");
+        bool isTrue = confirmDetails();
+        if (isTrue)
+        {
+            ofstream out;
+            out.open("contact-list.txt", ios_base::app);
+            out << "\n" << contactName << "\t\t\t\t\t\t";
+            for(int i = 0; i < no_of_contacts; i++)
+            {
+                out << mobileNumber[i] << " / ";
+            }
+        }
+        else    
+            cout << "Error";
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include "create-contact.h"
 using namespace std;
 
 int main()
 {
-    system("cls");
-    int choice;
-    cout << "PHONEBOOK MANAGEMENT SYSTEM\n\n";
-    cout << "Choose from the following operations: \n";
-    cout << "1. Add a Contact\n";
-    cout << "2. Remove a Contact\n";
-    cout << "3. Search a Contact\n";
-    cout << "4. Edit a Contact\n";
-    cout << "\nYour Choice: ";
-    cin >> choice;
+    while (1)
+    {
+        system("cls");
+        int choice;
+        cout << "PHONEBOOK MANAGEMENT SYSTEM\n\n";
+        cout << "Choose from the following operations: \n";
+        cout << "1. Add a Contact\n";
+        cout << "2. Remove a Contact\n";
+        cout << "3. Search a Contact\n";
+        cout << "4. Edit a Contact\n";
+        cout << "\nYour Choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            CreateContact contact;
+            contact.getDetails();
+            // bool isTrue = contact.confirmDetails();
+            contact.addContact();
+        }
+    }
     return 0;
 }
