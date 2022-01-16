cb271a154c9ff7a2c09e84578677114add2bd82c
Kush Agarwal <agarwalkushajay@gmail.com>
2022-01-16 13:53:13 +0530

    Added Main method

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!!" << endl;
+    system("cls");
+    int choice;
+    cout << "PHONEBOOK MANAGEMENT SYSTEM\n\n";
+    cout << "Choose from the following operations: \n";
+    cout << "1. Add a Contact\n";
+    cout << "2. Remove a Contact\n";
+    cout << "3. Search a Contact\n";
+    cout << "4. Edit a Contact\n";
+    cout << "\nYour Choice: ";
+    cin >> choice;
     return 0;
 }
